8d91e83e184b927dc501ab2b063e217f3067a9a7
velinamons <velinamons@gmail.com>
2024-12-17 11:39:00 +0200

    Refactor function parameters to be: path parmeter; required, with default and optional query parameters

--- main.py
+++ main.py
@@ -33,8 +33,11 @@ async def root():
 # with that type declaration, FastAPI gives automatic request "parsing" and data validation
 # All the data validation is performed under the hood by Pydantic
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 
 # Path operations are evaluated in order
