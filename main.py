28274c3ce5479592f09e426322fb570f00f85fcd
velinamons <velinamons@gmail.com>
2024-12-17 11:36:43 +0200

    Add path with query parameters with default value

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ class ModelName(str, Enum):
 app = FastAPI()
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# When you declare other function parameters that are not part of the path parameters
+# they are automatically interpreted as "query" parameters
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
