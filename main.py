5ae5fa8caea0618532e50decacc3fd10cbc90287
velinamons <velinamons@gmail.com>
2024-12-17 03:24:53 +0200

    Add path operation with predefined valid path parameter using Enum

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+# By inheriting from str the API docs will know that the values must be type string
+# and will be able to render correctly
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -25,3 +36,19 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+# path parameter with a type annotation using the enum class (ModelName)
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    # Compare enumeration members
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    # You can get the actual value using your_enum_member.value
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    # You can return enum members from your path operation
+    # They will be converted to their corresponding values before returning
+    return {"model_name": model_name, "message": "Have some residuals"}
