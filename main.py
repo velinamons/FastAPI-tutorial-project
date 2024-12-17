35e108a9efa0819f825141990893a2960b655c87
velinamons <velinamons@gmail.com>
2024-12-17 03:08:59 +0200

    Add path operation with type declaration

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# Path parameters with types (using Python type annotations)
+# with that type declaration, FastAPI gives automatic request "parsing" and data validation
+# All the data validation is performed under the hood by Pydantic
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
