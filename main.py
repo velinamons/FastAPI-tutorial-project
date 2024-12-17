ca11a238abcfccf10b3931f7f0bd4594d26e752a
velinamons <velinamons@gmail.com>
2024-12-17 03:10:18 +0200

    Add path operations with proper order

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+# Path operations are evaluated in order
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
